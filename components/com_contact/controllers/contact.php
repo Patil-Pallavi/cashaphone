01bda3241264d05dceea6c4323c1975dc5022d28
Pallavi Patil <pallavi.patil@wwindia.com>
2018-10-25 13:01:16 +0530

    contact controller modified

--- components/com_contact/controllers/contact.php
+++ components/com_contact/controllers/contact.php
@@ -222,8 +222,8 @@ class ContactControllerContact extends JControllerForm
 		// If we are supposed to copy the sender, do so.
 
 		// Check whether email copy function activated
-		if ($copy_email_activated == true && !empty($data['contact_email_copy']))
-		{
+		//if ($copy_email_activated == true && !empty($data['contact_email_copy']))
+		//{
 			$copytext    = JText::sprintf('COM_CONTACT_COPYTEXT_OF', $contact->name, $sitename);
 			$copytext    .= "\r\n\r\n" . $body;
 			$copysubject = JText::sprintf('COM_CONTACT_COPYSUBJECT_OF', $subject);
@@ -235,7 +235,7 @@ class ContactControllerContact extends JControllerForm
 			$mail->setSubject($copysubject);
 			$mail->setBody($copytext);
 			$sent = $mail->Send();
-		}
+		//}
 
 		return $sent;
 	}
